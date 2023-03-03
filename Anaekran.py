7be2aa1d2d258f7d4bf6f6961847218b480cbd9a
orkunumur <orkunumur@gmail.com>
2023-03-03 19:05:36 +0300

    arayüz güncellendi , ek metotlar tanımlandı, ama içerikleri yok

--- Anaekran.py
+++ Anaekran.py
@@ -47,6 +47,7 @@ class MainPage(QMainWindow):
         a=self.pizza_secim(self.pizza_tuple())
         return print(a)
     
+
     def pizza_tuple(self):
         s_pi= sade_pizza()
         t_pi= Turk_pizza()
@@ -60,6 +61,15 @@ class MainPage(QMainWindow):
         ]
         return pizzalar_tuple
     
+    def malzeme_tuple(self):
+        pass
+         
+    def soslar_tuple(self):
+        pass
+
+    def icecekler_tuple(self):
+        pass
+    
 
     def pizza_secim(self, pizza_listesi):
         for eleman in pizza_listesi:
@@ -67,14 +77,22 @@ class MainPage(QMainWindow):
                 self.siparis.append(eleman[0:2])
         return self.siparis
 
+
     def malzeme_secimi(self, malzeme_listesi):
         for eleman in malzeme_listesi:
             if eleman[2] == True:
                 self.siparis.append(eleman[0:2])
         return self.siparis
+    
+    def sos_secim(self, sos_listesi):
+        pass
+
+    def icecekler_secim(self, icecek_listesi):
+        pass
+
 
     def sozluk_olustur(siparis_listesi):
-        sepet_ekle = {"pizza": "", "malzeme": "", "fiyat": 0}
+        sepet_ekle = {"Pizza": "", "Malzemeler": "","Soslar":"", "İçecekler":"", "Fiyat": 0, "Notlar":""}
         for veri in siparis_listesi:
             if "Pizza" in veri[0]:
                 sepet_ekle['pizza'] = veri[0]
@@ -84,6 +102,7 @@ class MainPage(QMainWindow):
                 sepet_ekle["malzeme"] += veri[0]
             sepet_ekle["fiyat"] += veri[1]
         return sepet_ekle
+
 
     def checkBox_secim(self):
         check_box =[
@@ -106,30 +125,41 @@ class MainPage(QMainWindow):
                         other_checkbox[0].setEnabled(True)
      
 
-    def veri_ekle(self, veriler):  
+    def tabloya_veri_ekle(self, veriler):  
         table_widget = self.ui.sepet_table
         row_count = table_widget.rowCount()
         table_widget.setRowCount(row_count + 1)
         row = row_count
-        # yeni bir QTableWidgetItem objesi oluştur
-        pizza_item = QTableWidgetItem(veriler["pizza"])
-        malzeme_item = QTableWidgetItem(veriler["malzeme"])
-        fiyat_item = QTableWidgetItem(str(veriler["fiyat"]))
 
-        # QTableWidgetItem objelerinin ulaşılmaz yapılması
+        # yeni bir QTableWidgetItem objesi oluştur
+        pizza_item = QTableWidgetItem(veriler["Pizza"])
+        malzeme_item = QTableWidgetItem(veriler["Malzemeler"])
+        sos_item = QTableWidgetItem(veriler["Soslar"])
+        icecekler_item = QTableWidgetItem(veriler["İçecekler"])
+        tutar_item = QTableWidgetItem(str(veriler["Fiyat"]))
+        notlar_item = QTableWidgetItem(veriler["Notlar"])
+
+        # Tablonun dışarıdan erişilmesini engelleme
         pizza_item.setFlags(pizza_item.flags() ^ Qt.ItemIsEditable)
         malzeme_item.setFlags(malzeme_item.flags() ^ Qt.ItemIsEditable)
-        fiyat_item.setFlags(fiyat_item.flags() ^ Qt.ItemIsEditable)
+        sos_item.setFlags(sos_item.flags() ^ Qt.ItemIsEditable)
+        icecekler_item.setFlags(icecekler_item.flags() ^ Qt.ItemIsEditable)
+        tutar_item.setFlags(tutar_item.flags() ^ Qt.ItemIsEditable)
+        notlar_item.setFlags(notlar_item.flags() ^ Qt.ItemIsEditable)
+
 
         # QTableWidgetItem objelerini tableWidget'a ekle
         table_widget.setItem(row, 0, pizza_item)
         table_widget.setItem(row, 1, malzeme_item)
-        table_widget.setItem(row, 2, fiyat_item)
-
-        # Checkbox ekle
+        table_widget.setItem(row, 2, sos_item)
+        table_widget.setItem(row, 3, icecekler_item)
+        table_widget.setItem(row, 4, tutar_item)
+        table_widget.setItem(row, 5, notlar_item)
+        
+        #  Sil Sütununa Checkbox eklenmesi
         checkbox_item = QTableWidgetItem()
         checkbox_item.setCheckState(Qt.Checked)
-        table_widget.setItem(row, 3, checkbox_item)
+        table_widget.setItem(row, 6, checkbox_item)
 
 
 
